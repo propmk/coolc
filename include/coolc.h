bbab7920403e75419e77b93084b3e07a15baf8d4
Protyay Kumar Pramanik <88667795+propmk@users.noreply.github.com>
2025-09-13 20:28:40 +0600

    Added option for drawing title box and menu box

--- include/coolc.h
+++ include/coolc.h
@@ -2,7 +2,40 @@
 #define COOLC_H
 
 #include <stdio.h>
+#include <string.h>
 
+static void drawTitleBox(const char *title) {
+    int len = strlen(title);
 
+    printf("┌");
+    for (int i = 0; i < len + 2; i++) printf("─");
+    printf("┐\n");
+
+    printf("│ %s │\n", title);
+
+    printf("└");
+    for (int i = 0; i < len + 2; i++) printf("─");
+    printf("┘\n");
+}
+
+static void drawMenu(const char *options[], int n) {
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        int len = strlen(options[i]) + 3;
+        if (len > maxLen) maxLen = len;
+    }
+
+    printf("┌");
+    for (int i = 0; i < maxLen + 3; i++) printf("─");
+    printf("┐\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("│ [%d] %-*s │\n", i + 1, maxLen - 3, options[i]);
+    }
+
+    printf("└");
+    for (int i = 0; i < maxLen + 3; i++) printf("─");
+    printf("┘\n");
+}
 
 #endif 
